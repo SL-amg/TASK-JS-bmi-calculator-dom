72efa85919724b237be27380f9d9bb638aba3bc7
Abdullah Abbas <sl_amg@msn.com>
2024-10-28 19:43:17 +0300

    BMI program

--- index.js
+++ index.js
@@ -8,4 +8,35 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+let BMI = 0;
+BMI = ((weight)/((height/100)^2));
+console.log(BMI);
+
+let a = 18.5;
+let b = 24.9;
+let c = 25.0
+let d = 29.9;
+let e = 30.0;
+
+if (BMI <= a){
+  console.log("Underweight");
+  alert(BMI);
+  alert("Underweight")
+}
+else if (BMI > a && BMI < b){
+  console.log("Healthy Weight");
+  alert(BMI);
+  alert("Healthy Weight")
+}
+else if (BMI > c && BMI < d){
+  console.log("Overweight");
+  alert(BMI);
+  alert("Overweight")
+}
+else{
+  console.log("Above Obesity");
+  alert(BMI);
+  alert("Above Obesityt")
+}
+
 }
