a4c5bfd7c36f5db62a67d0517cac97e0a72e97ea
Abdullah Abbas <sl_amg@msn.com>
2024-10-28 20:34:09 +0300

    challange 3 done

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ function calculateBMI() {
 let BMI = 0;
 BMI = ((weight)/((height/100)^2));
 console.log(BMI);
-
-let a = 18.5;
+//** 
+/* let a = 18.5;
 let b = 24.9;
 let c = 25.0
 let d = 29.9;
 let e = 30.0;
-
-if (BMI <= a){
+**/
+//** remove this to see challange 1 and 2 solution 
+/* if (BMI <= a){
   console.log("Underweight");
   alert(BMI);
   alert("Underweight")
@@ -38,5 +39,27 @@ else{
   alert(BMI);
   alert("Above Obesityt")
 }
+**/
 
+if (age > 19 && age < 24 && BMI > 19 && BMI <24){
+  alert("Healthy");
+}
+else if (age > 25 && age < 34 && BMI > 20 && BMI <25){
+  alert(`Healthy - ${BMI}`);
+}
+else if (age > 35 && age < 44 && BMI > 21 && BMI <26){
+  alert(`Healthy - ${BMI}`);
+}
+else if (age > 45 && age < 54 && BMI > 22 && BMI <27){
+  alert(`Healthy - ${BMI}`);
+}
+else if (age > 55 && age < 64 && BMI > 23 && BMI <28){
+  alert(`Healthy - ${BMI}`);
+}
+else if (age > 65 &&  BMI > 24 && BMI <29){
+  alert(`Healthy - ${BMI}`);
+}
+else {
+  alert(`Not Healthy - ${BMI}`);
+}
 }
